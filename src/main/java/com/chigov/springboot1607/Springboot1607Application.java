3803bfca5652bd75b59c9070989d7e13ea51a93e
chigov2 <chigov@gmail.com>
2022-07-16 19:50:25 +0300

    Initial commit

---
+++ src/main/java/com/chigov/springboot1607/Springboot1607Application.java
@@ -0,0 +1,13 @@
+package com.chigov.springboot1607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1607Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1607Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chigov/springboot1607/Springboot1607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chigov.springboot1607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1607ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
